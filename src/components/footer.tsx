1602e994cb83467e217a2d9eb9678504067f42da
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2025-12-23 11:38:57 +0900

    add mastodon link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { Icons } from "./icons";
+import { SiMastodon } from "@icons-pack/react-simple-icons";
+
 export default function Footer() {
   return (
     <div className="mx-auto max-w-screen-md px-4 md:px-8 flex justify-end pb-[100px]">
@@ -12,6 +14,11 @@ export default function Footer() {
       <Link href="https://instagram.com/tokuzou0829" target="_blank">
         <div className="bg-white hover:bg-slate-100 rounded p-[10px] mr-[10px]">
           <Icons.instagram />
+        </div>
+      </Link>
+      <Link href="https://fedi.yutakobayashi.com/@tkz" rel="me" target="_blank">
+        <div className="bg-white hover:bg-slate-100 rounded p-[10px] mr-[10px]">
+          <SiMastodon />
         </div>
       </Link>
       <Link href="https://github.com/tokuzou0829" target="_blank">
